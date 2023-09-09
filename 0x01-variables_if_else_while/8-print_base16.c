b1c5a06c9cc3644eb8a85cf0898d0960fda8eced
lSaifmostafal <saifmostafa270@gmail.com>
2023-09-08 17:26:20 -0700

    a

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C CODE
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int digit = 4
+
+	while (digit <= 102)
+	{
+		putchar(digit);
+		if (digit == 57)
+			digit += 39;
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+
+}
