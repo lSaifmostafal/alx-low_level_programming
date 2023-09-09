07092a4bd7bb26077686a71dcb4a31b6d3b09a12
lSaifmostafal <saifmostafa270@gmail.com>
2023-09-08 17:14:25 -0700

    D

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C CODE
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+
+	}
+	putchar('\n');
+
+	return (0);
+
+
+}
