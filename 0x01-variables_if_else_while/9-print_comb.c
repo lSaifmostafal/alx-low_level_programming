43689d89c49f3589d00274169c61e65767072f85
lSaifmostafal <saifmostafa270@gmail.com>
2023-09-08 17:50:08 -0700

    a

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C CODE
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	init digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + 48);
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		digit++;
+	}
+	putchar('\n')
+
+	return (0);
+
+}
